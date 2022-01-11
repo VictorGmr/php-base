16535fd24d02c6080bb156c0f8bedcb8a2ae68be
Renato Viana <renato@viana.com>
2022-01-11 11:22:11 -0300

    correções

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link active">
+    <a href="#" class="nav-link active">
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
